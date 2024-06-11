24be38f2a0907c25c9da120474b24a5d6270f9d9
Thinkedou <r.enguehard@gmail.com>
2024-06-11 10:33:00 +0200

    feat: edit/create mode on CreateUpdateBooks.vue

--- src/views/BooksList.vue
+++ src/views/BooksList.vue
@@ -2,11 +2,32 @@
 import {onBeforeMount} from 'vue'
 import {useBookStore} from '../stores/books.js'
 import { storeToRefs } from 'pinia'
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
 
 const bookStore = useBookStore()
 const {fetchAllBooks} = bookStore
 const {books}  = storeToRefs(bookStore)
 
+
+const handleCreateButton = ()=>{
+	router.push({
+		name:'booksCreateUpdate',
+		params:{
+			bookId:'new'
+		}
+	})
+}
+
+const handleEditButton = (bookId)=>{
+	router.push({
+		name:'booksCreateUpdate',
+		params:{
+			bookId
+		}
+	})
+}
 
 onBeforeMount(async ()=>{
 	const tryToFetch = await fetchAllBooks()
@@ -25,23 +46,25 @@ onBeforeMount(async ()=>{
 				
 				<header>
 					<h2>Liste Books</h2>
-					<p>Combien</p>
+					<button @click="handleCreateButton" class='mt-4 icon solid fas fa-book-medical'>ADD</button>
+					
 				</header>
 				<p>
 					<table>
 						<tr>
-							<th>Name</th>
-							<th>Type</th>
-							<th>Action</th>
+							<th>Title</th>
+							<th>Author</th>
+							<th>Edit</th>
+							<th>Delete</th>
 						</tr>
 						<tr
 							v-for="book in books"
 							:key="book._id"
 						>
 							<td>{{book.title}}</td>
-							<td>{{book.author.firstName}}</td>
-							<td><a href='create-form.html'><button class='fas fa-pencil-alt' ></button></a></td>
-							<td><button class='icon solid far fa-trash-alt'></button></td>
+							<td>{{book.author.firstName+' '+book.author.lastName}}</td>
+							<td><button @click="handleEditButton(book._id)" class='fas fa-pencil-alt' ></button></td>
+							<td><button class='solid far fa-trash-alt'></button></td>
 						</tr>
 					</table>
 				</p>
--- src/views/CreateUpdateBooks.vue
+++ src/views/CreateUpdateBooks.vue
@@ -1,26 +1,56 @@
+<script setup>
+import {ref,onBeforeMount,computed} from 'vue'
+import {useRoute} from 'vue-router'
+import {useBookStore} from '@/stores/books.js'
+import { storeToRefs } from 'pinia'
+
+const route = useRoute()
+const bookStore = useBookStore()
+
+const onEditMode = ref(false)
+
+
+const pageTitle = computed(()=>{
+	return onEditMode.value?'EDITION':'CREATION'
+})
+const btnLabel = computed(()=>{
+	return onEditMode.value?'update':'create'
+})
+
+onBeforeMount(()=>{
+	const {params} = route 
+	if(params.bookId==='new'){
+		onEditMode.value = false
+	}else{
+		onEditMode.value = true
+	}
+})
+
+
+</script>
+
 <template>
   <section id="main">
-	<div class="container">
+		<div class="container">
 		<!-- Content -->
-		<h2> Create/ Update Form</h2>
+			<h2 class="mb-2">{{ pageTitle }}</h2>
 
-			<form>
-				<div class="row gtr-50">
-					<div class="col-6 col-12-small">
-						<input name="name" placeholder="Name" type="text">
+				<form>
+					<div class="row gtr-50 ">
+						<div class="col-6 col-12-small">
+							<input name="name" placeholder="Name" type="text">
+						</div>
+						<div class="col-6 col-12-small">
+							<input name="email" placeholder="Email" type="text">
+						</div>
+						<div class="col-12">
+							<textarea name="message" placeholder="Message"></textarea>
+						</div>
+						<div class="col-12">
+							<a href="#" class="form-button-submit button icon solid fa-pen">{{btnLabel}}</a>
+						</div>
 					</div>
-					<div class="col-6 col-12-small">
-						<input name="email" placeholder="Email" type="text">
-					</div>
-					<div class="col-12">
-						<textarea name="message" placeholder="Message"></textarea>
-					</div>
-					<div class="col-12">
-						<a href="#" class="form-button-submit button icon solid fa-pen">Create/update</a>
-						
-					</div>
-				</div>
-			</form>
+				</form>
 
 		</div>
 	</section>
